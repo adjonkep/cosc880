701cbdb9c5b5f3d75fbf1e460fc7b806f4407e5f
Adrien Djonkep Touomo <atouomo@ad.hlsdev.local>
2018-11-09 10:49:24 -0600

    testing layout

--- track_processing.py
+++ track_processing.py
@@ -2,11 +2,16 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 monza_img = Image.open('C:/Users/atouomo/Downloads/monza.png')
 
 monza_fft = np.fft.fft(monza_img)
 
+
+np.set_printoptions(threshold=np.nan)
+
 monza_amplitude = np.abs(monza_fft/217)
-print(monza_amplitude)
+print(monza_fft[0])
+#print(len(monza_amplitude))
 #plt.plot(monza_amplitude)
 #plt.show()
